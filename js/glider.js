a029621ad3dd429aed2ee4ff9d16409044209334
Lbicvic <leopold.bicvic@gmail.com>
2022-08-10 16:15:35 +0200

    add carousel responsive - need fix

--- js/glider.js
+++ js/glider.js
@@ -2,5 +2,10 @@ new Glide('.glide', {
     type: 'carousel',
     startAt: 1,
     perView: 3,
-    peek: "100px"
+    peek: "100px",
+    breakpoints : {
+      600: {
+        perView: 1
+      }
+    }
   }).mount()
