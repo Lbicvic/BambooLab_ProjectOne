9df5a446a07eee9743932d724c46fa9c5f4c0731
Lbicvic <leopold.bicvic@gmail.com>
2022-08-12 21:15:44 +0200

    fix hamburger display

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,11 +1,15 @@
-let hamburger = document.getElementsByClassName('nav__hamburger-menu');
-let htmlNav = document.querySelector('nav')
-let htmlMain = document.querySelector('main')
-let htmlFooter = document.querySelector('footer')
-let menu = document.getElementsByClassName('hamburger-menu')
+const hamburger = document.getElementsByClassName('nav__hamburger-menu');
+const htmlNav = document.querySelector('nav')
+const htmlMain = document.querySelector('main')
+const htmlFooter = document.querySelector('footer')
+const menu = document.getElementsByClassName('hamburger-menu')
+const html = document.querySelector('html')
+const htmlBody = document.querySelector('body')
 
-for (let i=0; i<2; i++) {
-    hamburger[i].addEventListener('click',()=>{
+for (let i = 0; i < 2; i++) {
+    hamburger[i].addEventListener('click', () => {
+        html.style.height = "100%";
+        htmlBody.style.height = "100%";
         htmlNav.classList.toggle("hidden-body")
         htmlMain.classList.toggle("hidden-body")
         htmlFooter.classList.toggle("hidden-body")
